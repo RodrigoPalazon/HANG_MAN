6c7c0e9ba083d7a7a750066b0ae392298f046dbb
Rodrigo Palazon <palazonbatera@gmail.com>
2024-06-25 12:08:23 +0200

    Add <Hangman> props to 'Apps.tsx' file

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import Hangman from "./Components/Hangman";
+import Keyboard from "./Components/Keyboard";
+import WordtoGuess from "./Components/WordtoGuess";
+import Message from "./Components/Message"; 
 
-function App() {
+const App = () => {
 const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
 });
 
-    return <>
-        <p>{wordToGuess}</p>
-  </>
+const [endOfTheGame, setEndOfTheGame] = useState<boolean>(false)
+
+const [guessedLettersList, setGuessedLetters] = useState<string[]>([]);
+
+
+    return (
+        <div style={{
+            maxWidth: "800px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2em",
+            margin: "0 auto",
+            alignItems: "center",
+            backgroundColor: "pink"
+        }}
+        >
+            
+            <Message endOfTheGame = {endOfTheGame}/>
+            <Hangman numberOfGuesses = {guessedLettersList.length}/>
+            <WordtoGuess word={wordToGuess}/>
+            <Keyboard/>
+      </div>
+    )
 }
 
 export default App
