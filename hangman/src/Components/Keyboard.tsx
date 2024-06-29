2d4c48123011cc5a1ce64796d5ccb0a8b8cd9baf
Rodrigo Palazon <palazonbatera@gmail.com>
2024-06-29 16:39:43 +0200

    Set Keyboard Style

--- hangman/src/Components/Keyboard.tsx
+++ hangman/src/Components/Keyboard.tsx
@@ -1,12 +1,24 @@
 import alphabet from "../alphabet.json"
+import styles from  "../Keyboard.module.css"
+
+const handleClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    const spanElement = event.currentTarget;
+    const innerText = spanElement.innerText;
+    console.log(innerText);
+    // Perform any additional actions with the innerText
+  };
 
 const Keyboard =  () => {
 
-    return <>
+    return <div style={{display: "flex",
+                        flexWrap: "wrap",
+                        gap: ".2em",
+                        fontSize: "2em"
+    }}>
         {alphabet.map((letter) => {
-            return <p>{letter}</p>
+            return <button className= {styles.btn} onClick={(handleClick)} >{letter}</button>
         })}
-    </>
+    </div>
 }
 
 export default Keyboard
