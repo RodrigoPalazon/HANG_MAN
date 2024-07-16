51a7c3f1f85fc1192b602a6dc26d3a8bc2d80551
Rodrigo Palazon <palazonbatera@gmail.com>
2024-07-16 15:03:47 +0200

    Update: Keyboard buttons style behavior

--- hangman/src/Components/Keyboard.tsx
+++ hangman/src/Components/Keyboard.tsx
@@ -1,20 +1,21 @@
 import alphabet from "../alphabet.json"
 import styles from  "../Keyboard.module.css"
 
-const handleClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-    const spanElement = event.currentTarget;
-    const innerText = spanElement.innerText;
-    console.log(innerText);
-    // Perform any additional actions with the innerText
-  };
+// const handleClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+//     const spanElement = event.currentTarget;
+//     const innerText = spanElement.innerText;
+//     console.log(innerText);
+//     // Perform any additional actions with the innerText
+//   };
 
 type KeyboardProps = {
-    activeLetter: string[], 
+    disabled?: boolean,
+    activeLetters: string[], 
     inactiveLetters: string[], 
     addGuessedLetter: (letter: string) => void
 }
 
-const Keyboard =  ({activeLetter, inactiveLetters, addGuessedLetter} : KeyboardProps) => {
+const Keyboard =  ({disabled = false, activeLetters, inactiveLetters, addGuessedLetter} : KeyboardProps) => {
 
     return <div style={{display: "grid",
                         gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
@@ -22,8 +23,11 @@ const Keyboard =  ({activeLetter, inactiveLetters, addGuessedLetter} : KeyboardP
                         
     }}>
         {alphabet.map((key) => {
-            return <button className= {`${styles.btn}`} 
+            const isActive = activeLetters.includes(key)
+            const isInactive = inactiveLetters.includes(key)
+            return <button className= {`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`} 
                             onClick={() => addGuessedLetter(key)} 
+                            disabled= {isInactive || isActive || disabled}
                             key={key}>
                                 {key}
                     </button>
