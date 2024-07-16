eb14ff0c13eae6003275b1f9f10b4b7894036381
Rodrigo Palazon <palazonbatera@gmail.com>
2024-07-16 15:05:16 +0200

    Set: automatically select the next word to be guessed

--- hangman/src/Components/WordtoGuess.tsx
+++ hangman/src/Components/WordtoGuess.tsx
@@ -1,9 +1,10 @@
 type WordtoGuessType = {
+    reveal?: boolean,
     word: string;
     guessedLetter: Array<string>; // or you can use guessedLetter: string[];
 };
 
-const WordtoGuess = ({word, guessedLetter} : WordtoGuessType) => {
+const WordtoGuess = ({reveal = true, word, guessedLetter} : WordtoGuessType) => {
     return <div style={{display: "flex",
         gap: ".25em",
         fontSize: "6rem",
@@ -14,7 +15,8 @@ const WordtoGuess = ({word, guessedLetter} : WordtoGuessType) => {
         
             {word.split("").map((letter, index) => (
                 <span style={{borderBottom: ".1em solid black"}} key={index}>
-                    <span style={{visibility: guessedLetter.includes(letter) ? "visible" : "hidden"}}>
+                    <span style={{visibility: guessedLetter.includes(letter) || reveal ? "visible" : "hidden", 
+                                    color: !guessedLetter.includes(letter) && reveal ? "red" : "black"}}>
                         {letter}
                     </span>
                 </span>
